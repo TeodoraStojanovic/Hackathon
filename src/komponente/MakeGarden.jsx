9f32de6dc1e38974a97aa3c8f1c4df07c276c26c
Teodora Stojanovic <teacine6@gmail.com>
2023-04-09 19:39:31 +0200

    commit

--- src/komponente/MakeGarden.jsx
+++ src/komponente/MakeGarden.jsx
@@ -3,22 +3,19 @@ import Cart from "./Cart";
 function MakeGarden({veg,kriterijum,add}){
 
     return(
+      <div className="kontejner">
         <div className="row-garden">
         {kriterijum==""
         ?
-        veg.map((f)=> <div  key={f.id} className="col-sm-3"><Cart add={add} key={f.id} f={f} mod={1}></Cart><br /></div>)
+        veg.map((f)=> <div  key={f.id} className="kartica"><Cart add={add} key={f.id} f={f} mod={1}></Cart><br /></div>)
         :
         <>
-        {veg
-        .filter((f)=>f.naziv.toLowerCase().includes(kriterijum.toLowerCase()))
-        .map((f)=> <div  key={f.id} className="col-sm-3"><Cart add={add}  key={f.id} f={f} mod={1}></Cart><br /></div>)}
-
         </>
         }
-           
+           </div>
       </div>
 
-
+    
     );
 
 }
