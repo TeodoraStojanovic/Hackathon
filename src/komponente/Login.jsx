760da2bb93fef7bcfaf3f0550d360a54106ed3e1
Ana Tosic <at20190073@student.fon.bg.ac.rs>
2023-04-12 23:10:52 +0200

    login, register

--- src/komponente/Login.jsx
+++ src/komponente/Login.jsx
@@ -3,66 +3,104 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 
-    const Login=(props)=>{
-        let history = useNavigate();
-        const [data, setData]=useState({
-            email:"",
-            password:""
+    const Login=()=>{
+        const[userData, setUserData]= useState({
+            email: " ",
+            password: " ",
         })
-
-        const handleChange=(e)=>{
-            setData({ ...data, [e.target.name]: e.target.value}); 
+        let navigate= useNavigate();
+        function handleInput(e){
+            //console.log(e);
+            let newUserData= userData;
+            newUserData[e.target.name]= e.target.value;
+            //console.log(newUserData);
+            setUserData(newUserData);
         }
-
-        const submitForm=(e)=>{
+        function handleLogin(e){
             e.preventDefault();
-        const sendData= {
-                email:data.email,
-                password:data.password
-            }
-
-            axios.post('http://127.0.0.1:8000/api/register', sendData).then((result)=>{
-                if(result.data.Status== 'Invalid'){
-                    alert('Invalid User');
-                }
-                else {
-                    history('/Login');
+            axios
+            .post("http://127.0.0.1:8081/api/login", userData)
+            .then((res)=>{
+                console.log(res.data);
+                if(res.data.success === true){
+                    window.sessionStorage.setItem("auth_token", res.data.access_token);
+                    navigate("/");
                 }
             })
+            .catch((e)=>{
+                console.log(e);
+            });
         }
-        return(
         
-            <div className="main-boxL">
-                <form onSubmit={submitForm} className="forma">
-                <div className="row">
-                    <div className="login"><h1>Log in</h1></div>
-                </div>
+        return(
+            <section
+            className="vh-100"
+            style={{paddingTop:4.5 + "rem",}}
+            >
+                <div className="conteiner-fluid h-custom">
+                    <div className="row d-flex justify-content-centar align-items-center h-100">
+                        <div className="col-md-9 col-lg-6 col-xl-5">
+                            <img
+                           src="https://media.istockphoto.com/id/466175630/photo/tomato-isolated-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=zloJA_rdhqA7aJ9O0mDDaBQOzOei3SlW2RCwvH4CxOA="
+                            className="img-fluid"
+                            style={{ width: "200px", height: "auto" }}
+                            alt="Sample image"
+                            />
+                            </div> 
+                            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                                <form onSubmit={handleLogin}>
+                                <div className="form-outline mb-4">
+                                    <input
+                                    type="email"
+                                    id="form3Exemple3"
+                                    className="form-control form-control-lg"
+                                    
+                                    name="email"
+                                    onInput={(e)=>handleInput(e)}
+                                    />
+                                    <label className="form-label" htmlFor="form3Exemple3">
+                                        Email address
+                                    </label>
+                                    </div>
+                                    <div className="form-outline mb-3">
+                                    <input
+                                    type="password"
+                                    id="form3Exemple4"
+                                    className="form-control form-control-lg"
+                                   
+                                    name="password"
+                                    onInput={(e)=>handleInput(e)}
+                                    />
+                                    <label className="form-label" htmlFor="form3Exemple4">
+                                        Password
+                                    </label>
+                                    </div>
 
-              
-                <div className="row">
-                <div className="col-md-6">Email</div>
-                <div className="col-md-6">
-                    <input type="email" name="email" className="form-control"
-                    onChange={handleChange} value={data.email}/>
-                </div>
-                </div>
+                                    <div className="text-center text-ig-start mt-4 pt-2">
+                                    <button
+                                    type="submit"
+                                
+                                    className="btn btn-primary btn-lg"
+                                    style={{
+                                        paddingLeft: 2.5 + "rem",
+                                        paddingRight: 2.5 + "rem",
+                                    }}
+                                    >
+                                    Log in
+                                    </button>
+                                    <p className="small fw-bold mt-2 pt-1 mb-0">
+                                        Don't have an account? {" "}
+                                        <a href="/register" className="link-danker">
+                                            Register
+                                        </a>
+                                    </p>
+                                </div>
+                                </form>
+                            </div>
 
-                <div className="row">
-                <div className="col-md-6">Password</div>
-                <div className="col-md-6">
-                    <input type="password" name="password" className="form-control"
-                    onChange={handleChange} value={data.password}/>
-                </div>
-                </div>
-
-
-                <div className="row">
-                    <div className="col-md-12 text-center">
-                    <input type="sumbit" name="sumbit" readOnly value="Log in" className="btn-login" />
-                    </div>
-                </div>
-                </form>
-            </div>
+                        </div>
+                  </div>
+            </section>        
             
         );
     }
--- src/komponente/Register.jsx
+++ src/komponente/Register.jsx
@@ -3,9 +3,9 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 
-    const Register=(props)=>{
-        let history = useNavigate();
-        const [data, setData]=useState({
+    const Register=()=>{
+        
+        const [userData, setUserData]=useState({
             first_name:"",
             last_name:"",
             addres:"",
@@ -13,95 +13,142 @@ import axios from 'axios';
             email:"",
             password:""
         })
-
-        const handleChange=(e)=>{
-            setData({ ...data, [e.target.name]: e.target.value}); 
+        let navigate=useNavigate();
+        function handleInput(e){
+            //console.log(e);
+            let newUserData= userData;
+            newUserData[e.target.name]= e.target.value;
+            //console.log(newUserData);
+            setUserData(newUserData);
         }
 
-        const submitForm=(e)=>{
+        function handleRegister(e){
             e.preventDefault();
-        const sendData= {
-                first_name:data.first_name,
-                last_name:data.last_name,
-                addres:data.addres,
-                telephone:data.telephone,
-                email:data.email,
-                password:data.password
-            }
-
-            axios.post('http://127.0.0.1:8000/api/register', sendData).then((result)=>{
-                if(result.data.Status== 'Invalid'){
-                    alert('Invalid User');
-                }
-                else {
-                    history('/Login');
-                }
+       
+            axios.post('http://127.0.0.1:8081/api/register', userData).then((res)=>{
+               console.log(res.data);
+               navigate("/login");
+            })
+            .catch((e)=>{
+                console.log(e);
             })
         }
         return(
-        
-            <div className="main-box">
-                <form onSubmit={submitForm} className="forma">
-                <div className="row">
-                    <div className="register"><h1>Register</h1></div>
-                </div>
+            <section
+            className="vh-100"
+            style={{paddingTop:4.5 + "rem",}}
+            >
+                <div className="conteiner-fluid h-custom">
+                    <div className="row d-flex justify-content-centar align-items-center h-100">
+                        <div className="col-md-9 col-lg-6 col-xl-5">
+                            <img
+                           src="https://www.organiccentar.rs/media/images/articles/Rotkvice.jpg"
+                            className="img-fluid"
+                            style={{ width: "225px", height: "auto" }}
+                            alt="Sample image"
+                            />
+                            </div> 
+                            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                                <form onSubmit={handleRegister}>
+                                <div className="form-outline mb-1">
+                                    <input
+                                    type="first_name"
+                                    id="form3Exemple1"
+                                    className="form-control form-control-lg"
+                                    
+                                    name="first_name"
+                                    onInput={(e)=>handleInput(e)}
+                                    />
+                                    <label className="form-label" htmlFor="form3Exemple1">
+                                        First name
+                                    </label>
+                                    </div>
+                                    <div className="form-outline mb-2">
+                                    <input
+                                    type="last_name"
+                                    id="form3Exemple2"
+                                    className="form-control form-control-lg"
+                                    
+                                    name="last_name"
+                                    onInput={(e)=>handleInput(e)}
+                                    />
+                                    <label className="form-label" htmlFor="form3Exemple2">
+                                        Last name
+                                    </label>
+                                    </div>
+                                    <div className="form-outline mb-5">
+                                    <input
+                                    type="addres"
+                                    id="form3Exemple5"
+                                    className="form-control form-control-lg"
+                                    
+                                    name="addres"
+                                    onInput={(e)=>handleInput(e)}
+                                    />
+                                    <label className="form-label" htmlFor="form3Exemple5">
+                                        Address
+                                    </label>
+                                    </div>
+                                    <div className="form-outline mb-6">
+                                    <input
+                                    type="telephone"
+                                    id="form3Exemple6"
+                                    className="form-control form-control-lg"
+                                    
+                                    name="telephone"
+                                    onInput={(e)=>handleInput(e)}
+                                    />
+                                    <label className="form-label" htmlFor="form3Exemple6">
+                                        Telephone
+                                    </label>
+                                    </div>
+                                <div className="form-outline mb-4">
+                                    <input
+                                    type="email"
+                                    id="form3Exemple3"
+                                    className="form-control form-control-lg"
+                                    
+                                    name="email"
+                                    onInput={(e)=>handleInput(e)}
+                                    />
+                                    <label className="form-label" htmlFor="form3Exemple3">
+                                        Email address
+                                    </label>
+                                    </div>
+                                    <div className="form-outline mb-3">
+                                    <input
+                                    type="password"
+                                    id="form3Exemple4"
+                                    className="form-control form-control-lg"
+                                   
+                                    name="password"
+                                    onInput={(e)=>handleInput(e)}
+                                    />
+                                    <label className="form-label" htmlFor="form3Exemple4">
+                                        Password
+                                    </label>
+                                    </div>
 
-                <div className="row">
-                <div className="col-md-6">First Name</div>
-                <div className="col-md-6">
-                    <input type="text" name="first_name" className="form-control" 
-                    onChange={handleChange} value={data.first_name}/>
-                </div>
-                </div>
+                                    <div className="text-center text-ig-start mt-4 pt-2">
+                                    <button
+                                    type="submit"
+                                
+                                    className="btn btn-primary btn-lg"
+                                    style={{
+                                        paddingLeft: 2.5 + "rem",
+                                        paddingRight: 2.5 + "rem",
+                                    }}
+                                    >
+                                    Register
+                                    </button>
+                                   
+                                </div>
+                                </form>
+                            </div>
 
-                <div className="row">
-                <div className="col-md-6">Last Name</div>
-                <div className="col-md-6">
-                    <input type="text" name="last_name" className="form-control"
-                    onChange={handleChange} value={data.last_name} />
-                </div>
-                </div>
-                
-                <div className="row">
-                <div className="col-md-6">Addres</div>
-                <div className="col-md-6">
-                    <input type="text" name="addres" className="form-control"
-                    onChange={handleChange} value={data.addres} />
-                </div>
-                </div>
-
-                <div className="row">
-                <div className="col-md-6">Telephone</div>
-                <div className="col-md-6">
-                    <input type="text" name="telephone" className="form-control"
-                    onChange={handleChange} value={data.telephone} />
-                </div>
-                
-                </div>
-                <div className="row">
-                <div className="col-md-6">Email</div>
-                <div className="col-md-6">
-                    <input type="email" name="email" className="form-control"
-                    onChange={handleChange} value={data.email}/>
-                </div>
-                </div>
-
-                <div className="row">
-                <div className="col-md-6">Password</div>
-                <div className="col-md-6">
-                    <input type="password" name="password" className="form-control"
-                    onChange={handleChange} value={data.password}/>
-                </div>
-                </div>
-
-
-                <div className="row">
-                    <div className="col-md-12 text-center">
-                    <input type="sumbit" name="sumbit" readOnly value="Register" className="btn-register" />
-                    </div>
-                </div>
-                </form>
-            </div>
+                        </div>
+                  </div>
+            </section>        
             
         );
     }
